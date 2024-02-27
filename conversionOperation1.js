e0572065e04b4762666527c0d3838ea88ab8cf36
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-02-27 14:10:06 +0000

    conversion

---
+++ conversionOperation1.js
@@ -0,0 +1,58 @@
+//let score="33"
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+
+// let score1=null
+
+// let valueInNumber1=Number(score1)
+// console.log(typeof valueInNumbe1r);
+// console.log(valueInNumber1);
+
+//"33"=>3
+//"2233fghjk"=>NaN
+// true=>1; false=>0
+
+// let  isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn);
+
+//1=>true;  0=>false
+//"" =>false
+//"archana"=>true
+
+// let someNumber=222
+
+// let soNumber=String(someNumber)
+// console.log(soNumber);
+// console.log(typeof soNumber);
+
+
+
+//*********************Operations**********************
+
+// let value=2
+// let negvalue=-value
+// console.log(negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**2);
+//console.log(5%2);
+
+
+//console.log((3+5)*5%3);
+// console.log(+true);
+// console.log(+"");
+let gamecounter=100
+gamecounter++;
+
+console.log(gamecounter);
+
+
+
