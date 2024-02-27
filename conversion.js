291735f6b2efe99f05382c6ef4e73a3745d50cd6
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-02-27 11:48:43 +0000

    conversion

---
+++ conversion.js
@@ -0,0 +1,38 @@
+//let score="33"
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+
+// let score1=null
+
+// let valueInNumber1=Number(score1)
+// console.log(typeof valueInNumbe1r);
+// console.log(valueInNumber1);
+
+//"33"=>3
+//"2233fghjk"=>NaN
+// true=>1; false=>0
+
+let  isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn);
+
+//1=>true;  0=>false
+//"" =>false
+//"archana"=>true
+
+let someNumber=222
+
+let soNumber=String(someNumber)
+console.log(soNumber);
+console.log(typeof soNumber);
+
+
+
+
+
+
