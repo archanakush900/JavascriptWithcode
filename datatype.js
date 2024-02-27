e88b0f8ad10741e64d24ad3218857ab8f07431ef
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-02-27 16:17:35 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,38 @@
+//there are two typr of datatype
+
+//1.primitive
+//7type: String,Number, Boolean, null Undefined,Symbol,BigInt
+
+
+// const number1=1233
+// const isLoggedIn=false
+// const outsideTeam=null
+// let usereEmail;
+// const id=symbol('234')
+// const anotherid=symbol('234')
+// const Bigint=1234567890;
+
+
+
+//2.reference(Non primitive)
+//3type:Array,Object, Function
+
+// const heros=["tiger","darshan","jaya"]//array
+
+let myobj={
+   name:"archana",
+    Boolean:false,
+    age:55,
+ 
+
+
+}//object
+console.log(typeof myobj);
+
+//function ko javascript me variable k trh treat kr sakte hain
+// function declare
+//function(){}
+// const myFunction=function(){
+//     console.log("heloo")
+// }
+//function hi fun hi   return krta hai acutually me mana jata hai fun obj
