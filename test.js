e974c1e89a09a284de287673877f5094e7970d30
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-02-27 11:02:13 +0000

    data types

---
+++ test.js
@@ -0,0 +1,8 @@
+const accountId= 12344
+var NameId="archana"
+let paswaord="email@"
+let namear;
+
+acoountcity="jaipur"
+console.log(NameId)
+console.table([NameId,paswaord,accountId,namear])
---
+++ test2.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as newer version
+
+// alert(9+9) //we are useing node.js not browswer
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean
+//null=> standalone value
+//undefine=>
+//symbol=>unique
+//object
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+console.log(typeof "archana")
+
+
