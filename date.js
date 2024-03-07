9cd40b1a1947594fb27f655b7bc68c0271924600
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-03-07 19:22:49 +0000

    Part1 completed

---
+++ date.js
@@ -0,0 +1,31 @@
+//Dates
+// let mydates=new Date()
+// // console.log(mydates.toString());
+// console.log(mydates.toDateString);
+// // console.log(mydates.toLocaleString());
+// // console.log(typeof mydates);
+
+// let myCreatedDate=new Date(2023,0,23)
+let myCreatedDate=new Date("2024-00-14")
+
+
+
+// console.log(myCreatedDate.toLocaleDateString());
+
+// let myTimeStamp=Date.now()
+// // console.log(myTimeStamp);
+
+// // console.log( myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newdate=new Date()
+console.log(newdate);
+console.log(newdate.getMonth()+1);
+// `${newdate.getDay()}`
+
+
+
+
+
+
