dccd81b0307ebdcfc043e2445002c65dc386f553
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-03-23 17:46:37 +0000

    done

--- array.js
+++ array.js
@@ -72,9 +72,15 @@
 
 
 
-const array4=[1,2,3,[4,6,7,8],8,[7,8],[0,9]]
-const realarray=array4.flat(Infinity)
-console.log(realarray)
+// const array4=[1,2,3,[4,6,7,8],8,[7,8],[0,9]]
+// const realarray=array4.flat(Infinity)
+// console.log(realarray)
+
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
 
 
 
