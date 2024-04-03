4cb343246427bd0e77e33157dda5bfb009b2d8ff
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-04-03 18:50:58 +0000

    done

---
+++ fn1.js
@@ -0,0 +1,50 @@
+// function saymyname(){
+//     console.log("a")
+//     console.log("a")
+//     console.log("a")
+//     console.log("a")
+// }
+// //saymyname()
+
+// function addTwoNum(num1,num2){//parameters
+// console.log(num1+num2);
+// }
+
+
+//  const add= addTwoNum(3,5)
+//  console.log("add:", add)
+// addTwoNum(5,"4")
+// addTwoNum(5,null)//arguments
+
+// function addTwoNum(num1,num2){//parameters
+//    const result=num1+num2
+//    return result
+//     }
+//     const result=addTwoNum(5,7)
+//     console.log("Result:",result);
+
+//object pas krte hain fuction me 
+
+// const user={
+//    username:"hitesh",
+//    prices:199
+// }
+
+// function handleObject(anyobject){
+//    console.log(`Usernmae is ${anyobject.username} and price is ${anyobject.price}`);
+
+// }
+// //handleObject(user)
+// handleObject({
+//    username:"sam",
+//    price:399
+
+// })//also can do it
+
+//===============
+const myNewArray=[200, 5000, 300, 599, 35]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
