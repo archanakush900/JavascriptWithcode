5d6a0476203aa69467eb5f2fbc885a584e9ec639
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-04-03 19:20:39 +0000

    done

---
+++ scope.js
@@ -0,0 +1,18 @@
+// let a=10
+// const b=30
+// var c=20
+//{} this is scope it uses all programmimg lan
+//var c=300
+let a=399
+if(true){
+    let a=10
+const b=30
+
+   console.log("inner",a);
+
+}
+console.log(a);
+// console.log(b);
+// console.log(c);
+//=============================
+
