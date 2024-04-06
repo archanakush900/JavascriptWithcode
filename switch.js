f0a066da1ce801ac4880cdd4ea333c0d6913cba0
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-04-06 19:44:56 +0000

    done

---
+++ switch.js
@@ -0,0 +1,52 @@
+// switch (key){
+
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("fab");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("may");
+    break;
+  case 6:
+    console.log("june");
+    break;
+  case 7:
+    console.log("july");
+    break;
+  case 8:
+    console.log("august");
+    break;
+
+  case 9:
+    console.log("sep");
+    break;
+  case 10:
+    console.log("oct");
+    break;
+  case 11:
+    console.log("nov");
+    break;
+  case 12:
+    console.log("dec");
+    break;
+
+  default:
+    console.log("default case");
+    break;
+}
