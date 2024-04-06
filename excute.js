fa9469ca14224bc3ec41e5ff224119f23d93ab96
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-04-06 19:30:39 +0000

    done

---
+++ excute.js
@@ -0,0 +1,53 @@
+//js execution context
+//global 
+//single threded hai
+//1.global execution context
+//2. function execution context
+//3.eval execution context
+//[{}]2.memory creation phase
+//1.execution phase
+//1. global excution or global envirment
+ //if
+//  const isuserloggedIn=true
+
+//  if(isuserloggedIn){
+
+//  }//   
+//  //<,>,<=,>= ,==,!=,===
+//========
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// const balance=1000
+// if(balance>500) console.log("execute");//not good practice
+
+// const balance=1000
+// if(balance>500){
+//     console.log("less that");
+// }else if(balance<750){
+//     console.log("greater that 750");
+// }else if(balance<900){
+//     console.log("greater that 900");
+// }else{
+//     console.log("excute");
+// }
+
+//66666666666666666666666666666real life eg
+// const userLoggedIn=true
+// const debitedCard=true
+// const loggedFromGoogle=false
+// const loggedFromEmail=true
+// if(userLoggedIn&& debitedCard){
+//     console.log("Allow to buy couse");
+
+// }else{
+//     console.log("not Allowed to buy couses");
+// }
+// if(loggedFromGoogle||loggedFromEmail){
+//     console.log("allo to login");
+// }
+
+
