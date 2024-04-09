0511e532bf9f0bc7b6d8f13649560c11de45368e
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-04-09 08:31:22 +0000

    done

---
+++ true.js
@@ -0,0 +1,44 @@
+// const userEmail="archan65Gmail.com"
+// if(userEmail){
+//     console.log("Got user email");
+
+// }else{
+//     console.log("Don't have user email");
+// }
+// fasle values
+//false ,0,-0,BigINT 0n,"",null,undefined,NaN
+//truth  values
+
+//"0",'false'," ",[],{},function(){}
+
+// const userEmail=[]
+// if(userEmail.length===0){
+//     console.log("Array is empty");
+// }
+//========================
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+// console.log("aobj return");
+// }
+
+//=================================================
+// nullish coalescing operator(??)
+
+// let val1;
+// val1=5 ?? 10 output 5
+// val1=null ?? 67 output 67
+// val1=undefined ?? 15//output 15
+
+// val1 =null ?? 10 ?? 15
+
+// console.log(val1);
+
+//88888888888888888888888888888888888
+//terniary operator
+
+//condition ? true : false
+const iceTeaPrice=100
+iceTeaPrice >=80? console.log("less than 80"): console.log("more than 80")
+
+
