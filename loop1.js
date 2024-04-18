31a6afb51b469a26266150339c8fa9cfce537e75
Archana Maurya <110740466+archanakush900@users.noreply.github.com>
2024-04-18 17:13:18 +0000

    done1

---
+++ loop1.js
@@ -0,0 +1,21 @@
+//do while and while loop
+
+// let i=0
+// while(i<=10){
+//     console.log(`value of i is ${i}`);
+//     i=i+2
+
+
+// }
+
+// let myArray=['hari','shap','yash','jay']
+// let arr=0
+// while(arr<=myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr=arr+1
+// }
+let score=1
+do{
+    console.log(`score is ${score}`);
+score++;
+}while(score<=11);
